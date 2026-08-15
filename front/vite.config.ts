34daa3ded129551aed4626135cae8f7d6c119a3f
Trulio <tulio-santos@hotmail.com>
2026-08-15 08:35:08 -0300

    add gh page

---
+++ front/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Valkyrie-Profile-2-Translation/',
+})
